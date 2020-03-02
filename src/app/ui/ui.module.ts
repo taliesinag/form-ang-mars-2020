f596a11445e2debf1d783185bc4bf7c7a2b14e32
aymeric <aymeric.afpa2017@gmail.com>
2020-03-02 17:20:12 +0100

    Install libraries and install component

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { UiComponent } from './components/ui/ui.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [UiComponent, HeaderComponent, FooterComponent, NavComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule
+  ],
+  exports: [UiComponent]
 })
 export class UiModule { }
